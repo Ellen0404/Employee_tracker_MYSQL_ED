b79b6f703c9a217465725e48b9d7582c644bb36a
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-03-09 22:11:00 -0600

    Creating inquirer prompt and 3 tables in MYSQL

---
+++ develop/DB/seed.sql
@@ -0,0 +1,62 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+
+CREATE TABLE department
+(
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY
+    (id)
+);
+
+INSERT INTO department (id, name) VALUES (111,"Sales"); 
+INSERT INTO department (id, name) VALUES (222,"Engineering");
+INSERT INTO department (id, name) VALUES (333,"Finance");
+INSERT INTO department (id,name) VALUES (444,"Marketing");
+
+
+ CREATE TABLE role
+(
+  roleID INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (roleID),
+  FOREIGN KEY (department_id) REFERENCES department (id)
+  
+);
+
+INSERT INTO role (roleID, title, salary, department_id) VALUES (11,"Manager",170000,111); 
+INSERT INTO role (roleID, title, salary, department_id) VALUES (22,"Engineer",130000,222); 
+INSERT INTO role (roleID, title, salary, department_id) VALUES (33,"Sales lead",110000,111); 
+INSERT INTO role (roleID, title, salary, department_id) VALUES (44,"Saleperson",100000,111); 
+INSERT INTO role (roleID, title, salary, department_id) VALUES (55,"Accountant",85000,333); 
+INSERT INTO role (roleID, title, salary, department_id) VALUES (66,"Software Engineer",125000,222); 
+INSERT INTO role (roleID, title, salary, department_id) VALUES (77,"Marketologist",78000,444); 
+INSERT INTO role (roleID, title, salary, department_id) VALUES (88,"Lead Engineer",150000,222); 
+
+ CREATE TABLE employee
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name  VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id  INT,
+  manager_id INT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY (role_id) REFERENCES role (roleID)
+  
+);
+
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Bob","Collen",11,null); 
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sarah","Parker",22,null); 
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Dill","Tron",33,null); 
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Mike","Geler",44,null); 
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Rita","Obsman",55,null); 
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Nicol","Kidman",77,null); 
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Tom","Hender",88,null); 
+ INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Mila","Cohen",66,null); 
+
--- server.js
+++ server.js
@@ -1,3 +1,106 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var consoleTable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "0404abcdef",
+    database: "employeeDB"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+    //main function goes here :
+    takeAction();
+});
+
+function takeAction() {
+    inquirer.prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Empoyees by Department",
+            "View All Empoyees by Manager",
+            "View All Roles in a specific Department",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Remove Department",
+            "Remove Role",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "View the total utilized budget of a department",
+            "EXIT"
+
+        ]
+    }).then(function (answer) {
+        switch (answer.action) {
+            case "View All Employees":
+                //function goes under:
+                break;
+
+            case "View All Empoyees by Department":
+                //function goes under:
+                break;
+
+            case "View All Empoyees by Manager":
+                //function goes under:
+                break;
+
+            case "View All Roles in a specific Department":
+                //function goes under:
+                break;
+
+            case "Add Department":
+                //function goes under:
+                break;
+
+            case "Add Role":
+                //function goes under:
+                break;
+
+            case "Add Employee":
+                //function goes under:
+                break;
+
+            case "Remove Department":
+                //function goes under:
+                break;
+
+            case "Remove Role":
+                //function goes under:
+                break;
+
+            case "Remove Employee":
+                //function goes under:
+                break;
+
+            case "Update Employee Role":
+                //function goes under:
+                break;
+
+            case "Update Employee Manager":
+                //function goes under:
+                break;
+
+            case "View the total utilized budget of a department":
+                //function goes under:
+                break;
+
+            case "EXIT":
+                connection.end();
+                break;
+
+            default:
+                return "You will never get to this point)"
+
+        }
+    });
+}
