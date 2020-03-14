c654225478683a6ce8c55f0e9a44d438561e2f89
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-03-13 20:32:37 -0600

    all employees and budget

--- develop/DB/seed.sql
+++ develop/DB/seed.sql
@@ -7,39 +7,39 @@ USE employeeDB;
 
 CREATE TABLE department
 (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   PRIMARY KEY
     (id)
 );
 
-INSERT INTO department (id, name) VALUES (111,"Sales"); 
-INSERT INTO department (id, name) VALUES (222,"Engineering");
-INSERT INTO department (id, name) VALUES (333,"Finance");
-INSERT INTO department (id,name) VALUES (444,"Marketing");
+INSERT INTO department (name) VALUES ("Sales"); 
+INSERT INTO department (name) VALUES ("Engineering");
+INSERT INTO department (name) VALUES ("Finance");
+INSERT INTO department (name) VALUES ("Marketing");
 
 
  CREATE TABLE role
 (
-  roleID INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
   department_id INT,
-  PRIMARY KEY (roleID),
+  PRIMARY KEY (id),
   FOREIGN KEY (department_id) REFERENCES department (id)
   
 );
 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (11,"Manager",170000,111); 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (22,"Engineer",130000,222); 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (33,"Sales lead",110000,111); 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (44,"Saleperson",100000,111); 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (55,"Accountant",85000,333); 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (66,"Software Engineer",125000,222); 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (77,"Marketologist",78000,444); 
-INSERT INTO role (roleID, title, salary, department_id) VALUES (88,"Lead Engineer",150000,222); 
+INSERT INTO role (title, salary, department_id) VALUES ("Manager",170000,1); 
+INSERT INTO role (title, salary, department_id) VALUES ("Engineer",130000,2); 
+INSERT INTO role (title, salary, department_id) VALUES ("Sales lead",110000,1); 
+INSERT INTO role (title, salary, department_id) VALUES ("Saleperson",100000,1); 
+INSERT INTO role (title, salary, department_id) VALUES ("Accountant",85000,3); 
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer",125000,2); 
+INSERT INTO role (title, salary, department_id) VALUES ("Marketologist",78000,4); 
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer",150000,2); 
 
- CREATE TABLE employee
+ CREATE TABLE employees
 (
   id INT NOT NULL AUTO_INCREMENT,
   first_name  VARCHAR(30),
@@ -47,29 +47,46 @@ INSERT INTO role (roleID, title, salary, department_id) VALUES (88,"Lead Enginee
   role_id  INT,
   manager_id INT NULL,
   PRIMARY KEY(id),
-  FOREIGN KEY (role_id) REFERENCES role (roleID)
+  FOREIGN KEY (role_id) REFERENCES role (id)
   
 );
 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Bob","Collen",11,null); 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Sarah","Parker",22,null); 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Dill","Tron",33,null); 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Mike","Geler",44,null); 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Rita","Obsman",55,null); 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Nicol","Kidman",77,null); 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Tom","Hender",88,null); 
- INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Mila","Cohen",66,null); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Bob","Collen",1,null); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Sarah","Parker",2,7); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Dill","Tron",3,1); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Mike","Geler",4,3); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Rita","Obsman",5,1); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Nicol","Kidman",7,null); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Tom","Hender",8,null); 
+ INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("Mila","Cohen",6,7); 
 
 
-SELECT  employee.id, employee.first_name, employee.last_name,employee.manager_id, role.title, role.salary, department.name
-FROM employee 
-LEFT JOIN role ON employee.role_id=role.roleID
+SELECT  employees.id, employees.first_name, employees.last_name,employees.manager_id, role.title, role.salary, department.name
+FROM employees 
+LEFT JOIN role ON employees.role_id=role.id
 LEFT JOIN department ON role.department_id=department.id
-ORDER BY employee.id;
+ORDER BY employees.id;
 
 CREATE TABLE allEmployees AS 
-SELECT  employee.id, employee.first_name, employee.last_name,employee.manager_id, role.title, role.salary, department.name
-FROM employee 
-LEFT JOIN role ON employee.role_id=role.roleID
+SELECT  employees.id, employees.first_name, employees.last_name,employees.manager_id, role.title, role.salary, department.name
+FROM employees
+LEFT JOIN role ON employees.role_id=role.id
 LEFT JOIN department ON role.department_id=department.id
-ORDER BY employee.id;
+ORDER BY employees.id;
+
+---ALL EMPLOYEES--
+
+SELECT employees.id, employees.first_name, employees.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+ FROM employees LEFT JOIN role on employees.role_id = role.id
+ LEFT JOIN department on role.department_id = department.id 
+LEFT JOIN employees manager on manager.id = employees.manager_id;
+
+--BY MANAGER 
+SELECT employees.id, employees.first_name, employees.last_name, employees.manager_id, role.title, role.salary, role.department_id AS Department, CONCAT(m.first_name, " ", m.last_name) AS Manager 
+FROM employees
+LEFT JOIN role ON employees.role_id = role.id
+INNER JOIN department ON role.department_id = department_id 
+INNER JOIN employees m ON employees.manager_id = m.id WHERE CONCAT(m.first_name, " ", m.last_name) IN ('Bob Collen')
+GROUP BY employees.id;
+
+
--- server.js
+++ server.js
@@ -1,24 +1,10 @@
-var mysql = require("mysql");
+
 var inquirer = require("inquirer");
+var connection = require("./develop/JS/connection")
 const cTable = require('console.table');
-// var employeeName = require("./develop/JS/employeeName");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "0404abcdef",
-    database: "employeeDB"
-});
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    // console.log(employeeName);
-    //main function goes here :
-    takeAction();
-});
-
+takeAction();
 function takeAction() {
     inquirer.prompt({
         name: "action",
@@ -48,11 +34,12 @@ function takeAction() {
                 break;
 
             case "View All Empoyees by Department":
-                employeeByDepartment();
+                // employeeByDepartment();
                 break;
 
             case "View All Empoyees by Manager":
-                //function goes under:
+                employeeByManager();
+
                 break;
 
             case "Add Department":
@@ -64,7 +51,7 @@ function takeAction() {
                 break;
 
             case "Add Employee":
-                addEmployee();
+                addNewEmployee();
                 break;
 
             case "Remove Department":
@@ -76,7 +63,7 @@ function takeAction() {
                 break;
 
             case "Remove Employee":
-                removeEmployee();
+                // removeEmployee();
                 break;
 
             case "Update Employee Role":
@@ -88,7 +75,7 @@ function takeAction() {
                 break;
 
             case "View the total utilized budget of a department":
-                //function goes under:
+                budgetOfDepartment();
                 break;
 
             case "EXIT":
@@ -102,11 +89,13 @@ function takeAction() {
         }
     });
 }
-const query = "CREATE TABLE allEmployees AS SELECT  employee.id, employee.first_name, employee.last_name,employee.manager_id, role.title, role.salary, department.name FROM employee LEFT JOIN role ON employee.role_id=role.roleID LEFT JOIN department ON role.department_id=department.id ORDER BY employee.id;";
 
 
 function viewAllEmployees() {
-    var query = "SELECT * FROM employeeDB.allEmployees"
+    var query = "SELECT employees.id, employees.first_name, employees.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees";
+    query += " LEFT JOIN role on employees.role_id = role.id";
+    query += " LEFT JOIN department on role.department_id = department.id";
+    query += " LEFT JOIN employees manager on manager.id = employees.manager_id";
     connection.query(query, function (err, res) {
         if (err) throw err;
         console.table(res);
@@ -136,65 +125,125 @@ function employeeByDepartment() {
     })
 };
 
-function addEmployee() {
-    inquirer.prompt([
-        {
-            name: "employeeFirstName",
-            type: "input",
-            message: "What is the Employee's First name?"
-
-        },
-        {
-            name: "employeeLastName",
-            type: "input",
-            message: "What is the Employee's Last name?"
-        },
-        {
-            name: "employeeRole",
-            type: "input",
-            message: "What is the Employee's Role?"
-        }]).then(function (answer) {
-            var query = "INSERT INTO allEmployees SET ?";
-            var employeeAnswers = {
-                first_name: answer.employeeFirstName,
-                last_name: answer.employeeLastName,
-                title: answer.employeeRole
-            }
-            connection.query(query, employeeAnswers, function (err, res) {
-                if (err) throw err;
-                console.table(res);
-                takeAction();
-            });
-        })
-}
-
-function removeEmployee() {
-    var query = "SELECT CONCAT(first_name, ' ', last_name) AS fullName,id FROM allEmployees";
-    connection.query(query, function (err, res) {
-        console.table(res);
-        inquirer.prompt([{
-            name: "removeEmployee",
+function employeeByManager() {
+    connection.query(`SELECT CONCAT(m.first_name, " ", m.last_name) AS Manager, m.id FROM employees INNER JOIN employees m ON employees.manager_id = m.id`, function (err, res) {
+        inquirer.prompt({
+            name: "manager_id",
             type: "list",
-            message: "What Employee would you like to remove?",
-            choices:
-                function () {
-                    let dEmployee = [];
-                    for (i = 0; i < res.length; i++) {
-                        dEmployee.push(`${res[i].id}: ${res[i].fullName}`)
+            message: "By what Manager would you like to view all employees?",
+            choices: function () {
+                let managerArr = [];
 
-                    }
-                    return dEmployee;
+                for (i = 0; i < res.length; i++) {
+                    managerArr.push(`${res[i].id}: ${res[i].Manager}`);
+
                 }
 
+                return managerArr;
 
-        }]).then(function (answer) {
-            var query = `DELETE FROM allEmployees WHERE id = "${answer.removeEmployee[0]}"`;
-            connection.query(query, function (err, res) {
-                if (err) throw err;
-                console.log(`Employee ${answer.removeEmployee[0]} has been removed`);
-                takeAction();
-            });
+            }
+        }).then(function (answer) {
+            var query = `SELECT employees.id, CONCAT(first_name, " ", last_name) AS Name, role.title 
+            FROM employees INNER JOIN role ON employees.role_id = role.id 
+            WHERE employees.manager_id = ${answer.manager_id[0]} GROUP BY employees.id`;
+            connection.query(query, [answer.department],
+                function (err, result) {
+                    if (err) throw err;
+                    console.log("HELLLOOOO")
+                    console.table(result);
+                    takeAction();
+                });
         })
+    });
+}
+
+
+
+// function removeEmployee() {
+//     var query = "SELECT CONCAT(first_name, ' ', last_name) AS fullName,id FROM allEmployees";
+//     connection.query(query, function (err, res) {
+//         console.table(res);
+//         inquirer.prompt([{
+//             name: "removeEmployee",
+//             type: "list",
+//             message: "What Employee would you like to remove?",
+//             choices:
+//                 function () {
+//                     let dEmployee = [];
+//                     for (i = 0; i < res.length; i++) {
+//                         dEmployee.push(`${res[i].id}: ${res[i].fullName}`)
+
+//                     }
+//                     return dEmployee;
+//                 }
+
+
+//         }]).then(function (answer) {
+//             var query = `DELETE FROM allEmployees WHERE id = "${answer.removeEmployee[0]}"`;
+//             connection.query(query, function (err, res) {
+//                 if (err) throw err;
+//                 console.log(`Employee ${answer.removeEmployee[0]} has been removed`);
+//                 takeAction();
+//             });
+//         })
+//     });
+// };
+
+const addNewEmployee = () => {
+    connection.query(`SELECT CONCAT(first_name, " ", last_name) AS Manager, id FROM employees`, function (err, res) {
+        connection.query(`SELECT DISTINCT title, id from role`, function (err, data) {
+            inquirer.prompt([{
+                message: "What is the employee's first name?",
+                type: "input",
+                name: "first_name"
+            }, {
+                message: "What is the employee's last name?",
+                type: "input",
+                name: "last_name"
+            }, {
+                message: "What is the employee's role?",
+                type: "list",
+                name: 'role_id',
+                choices: function () {
+                    let roleArr = [];
+                    for (let i = 0; i < data.length; i++) {
+                        roleArr.push(`${data[i].id}: ${data[i].title}`);
+                    }
+                    return roleArr;
+                }
+            }, {
+                message: "Who will be this employee's Manager?",
+                type: "list",
+                name: 'manager_id',
+                choices: function () {
+                    let managerArr = [];
+                    for (let i = 0; i < res.length; i++) {
+                        managerArr.push(`${res[i].id}: ${res[i].Manager}`);
+                    }
+                    return managerArr;
+                }
+            }]).then(answer => {
+                connection.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ('${answer.first_name}', '${answer.last_name}', ${answer.role_id[0]}, ${answer.manager_id[0]})`, function (err, res) {
+                    if (err) throw err;
+                    console.log("--------------")
+                    console.log(`Employee ${answer.first_name} ${answer.last_name} has been added`);
+                    console.log("--------------")
+                    takeAction();
+                });
+            });
+        });
     });
 };
 
+function budgetOfDepartment() {
+    var query = "SELECT department.id, department.name, SUM(role.salary) AS utilized_budget FROM employees";
+    query += " LEFT JOIN role on employees.role_id = role.id";
+    query += " LEFT JOIN department on role.department_id = department.id";
+    query += " GROUP BY department.id, department.name";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        takeAction();
+
+    });
+}
