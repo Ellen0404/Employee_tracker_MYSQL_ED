fd74d8056779b85570a238813afd9c5b2b60ede6
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-03-14 16:49:49 -0600

    Add and Remove Role , View all Dep and all Roles

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ function takeAction() {
             "View All Employees",
             "View All Empoyees by Department",
             "View All Empoyees by Manager",
+            "View All Departments",
+            "View All Roless",
             "Add Department",
             "Add Role",
             "Add Employee",
@@ -42,12 +44,22 @@ function takeAction() {
 
                 break;
 
+            case "View All Departments":
+                allDepartment();
+
+                break;
+
+            case "View All Roless":
+                allRoles();
+
+                break;
+
             case "Add Department":
                 addDepartment();
                 break;
 
             case "Add Role":
-                //function goes under:
+                addRole();
                 break;
 
             case "Add Employee":
@@ -152,6 +164,25 @@ function employeeByManager() {
     });
 }
 
+function allDepartment() {
+    var query = "SELECT * FROM department";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        takeAction();
+    });
+}
+function allRoles() {
+    var query = `SELECT role.id,role.title, role.salary, department.name AS Department
+     FROM role 
+     LEFT JOIN department ON department.id =role.department_id
+    ORDER BY role.id `;
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        takeAction();
+    });
+}
 function addDepartment() {
     inquirer.prompt([{
         message: "What department would you like to add?",
@@ -168,6 +199,67 @@ function addDepartment() {
 
 }
 
+function addRole() {
+    connection.query("SELECT * FROM  department", function (req, res) {
+        inquirer.prompt([{
+            message: "What is going to be a new Role Title?",
+            type: "input",
+            name: "newTitle"
+        }, {
+            message: "What is going to be a Salary for that Role?",
+            type: "input",
+            name: "newSalary"
+        }, {
+            message: "To what Department would you like to assign this new Role?",
+            type: "list",
+            name: "roleDepartID",
+            choices: res.map(item => ({ name: item.name, value: item.id }))
+
+        }]).then(answer => {
+            connection.query(`INSERT INTO role(title, salary,department_id) VALUES ('${answer.newTitle}', '${answer.newSalary}',${answer.roleDepartID})`, function (err, res) {
+                if (err) throw err;
+
+                takeAction();
+            });
+        });
+    });
+}
+const addNewEmployee = () => {
+    connection.query(`SELECT CONCAT(first_name, " ", last_name) AS Manager, id FROM employees`, function (err, res) {
+        connection.query(`SELECT DISTINCT title, id from role`, function (err, data) {
+            inquirer.prompt([{
+                message: "What is the employee's first name?",
+                type: "input",
+                name: "first_name"
+            }, {
+                message: "What is the employee's last name?",
+                type: "input",
+                name: "last_name"
+            }, {
+                message: "What is the employee's role?",
+                type: "list",
+                name: 'role_id',
+                choices: data.map(o => ({ name: o.title, value: o.id }))
+
+            }, {
+                message: "Who will be this employee's Manager?",
+                type: "list",
+                name: 'manager_id',
+                choices: res.map(o => ({ name: o.Manager, value: o.id }))
+
+
+            }]).then(answer => {
+                connection.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ('${answer.first_name}', '${answer.last_name}', ${answer.role_id}, ${answer.manager_id})`, function (err, res) {
+                    if (err) throw err;
+                    console.log("--------------")
+                    console.log(`Employee ${answer.first_name} ${answer.last_name} has been added`);
+                    console.log("--------------")
+                    takeAction();
+                });
+            });
+        });
+    });
+};
 function removeDepartment() {
     connection.query("SELECT name, id FROM  department", function (err, res) {
 
@@ -228,42 +320,7 @@ function removeEmployee() {
     });
 };
 
-const addNewEmployee = () => {
-    connection.query(`SELECT CONCAT(first_name, " ", last_name) AS Manager, id FROM employees`, function (err, res) {
-        connection.query(`SELECT DISTINCT title, id from role`, function (err, data) {
-            inquirer.prompt([{
-                message: "What is the employee's first name?",
-                type: "input",
-                name: "first_name"
-            }, {
-                message: "What is the employee's last name?",
-                type: "input",
-                name: "last_name"
-            }, {
-                message: "What is the employee's role?",
-                type: "list",
-                name: 'role_id',
-                choices: data.map(o => ({ name: o.title, value: o.id }))
 
-            }, {
-                message: "Who will be this employee's Manager?",
-                type: "list",
-                name: 'manager_id',
-                choices: res.map(o => ({ name: o.Manager, value: o.id }))
-
-
-            }]).then(answer => {
-                connection.query(`INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES ('${answer.first_name}', '${answer.last_name}', ${answer.role_id}, ${answer.manager_id})`, function (err, res) {
-                    if (err) throw err;
-                    console.log("--------------")
-                    console.log(`Employee ${answer.first_name} ${answer.last_name} has been added`);
-                    console.log("--------------")
-                    takeAction();
-                });
-            });
-        });
-    });
-};
 
 // function updateRole (){
 
